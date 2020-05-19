3862a3f21ed0155494c5c5553b49dd058f0260c4
ReadyHash <Readyhash@gmail.com>
2020-05-19 15:55:05 +0800

    Added routes for posts -Shane

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/', to: 'posts#index'
+
+  #This page shows all posts
+  get '/posts', to: 'posts#index' as: 'posts'
+  get '/posts/new', to: 'posts#new' as: 'post_new'
+  post '/posts/new', to: 'posts#create' as: 'post_create'
+
+
 end
