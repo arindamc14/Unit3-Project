871e08d8bd5786ec877d8f39d40e634b20a7e58a
arindamc14 <arindamchowdhury91@gmail.com>
2020-05-20 13:23:27 +0800

    fixed the models and the seed file

--- app/models/pet_status.rb
+++ app/models/pet_status.rb
@@ -1,4 +1,7 @@
-class PetStatus < ActiveRecord::Base
+class PetStatus < ApplicationRecord
+
+	has_many :post	
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
