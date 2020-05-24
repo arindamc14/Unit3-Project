a293d5b036f8be87416f7caec59e58f4c33059b8
arindamc14 <arindamchowdhury91@gmail.com>
2020-05-25 04:53:47 +0800

    adding messaging feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :conversations do
+    resources :messages
+  end
 
   root 'posts#index'
 
@@ -22,5 +25,8 @@ Rails.application.routes.draw do
   patch '/posts/:id', to: 'posts#update'
 
   delete '/posts/:id', to: 'posts#delete', as: 'post_delete'
+
+
+  # get '/conversations', to: 'conversations#index',
 
 end
