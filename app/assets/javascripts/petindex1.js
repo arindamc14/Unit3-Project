21af30980838fb2e6a6e3742a380dc8315da2731
YANNIEYEUNG <K_T_DINO@HOTMAIL.COM>
2020-05-25 12:11:38 +0800

    index page css completed

---
+++ app/assets/javascripts/petindex1.js
@@ -0,0 +1,8 @@
+var animation = bodymovin.loadAnimation({
+  container: document.querySelector(".indexlostpet"),
+  renderer: "svg",
+  loop: true,
+  autoplay: true,
+  //   path: "public/sgSkyline-final.json",
+  path: "https://assets7.lottiefiles.com/packages/lf20_zn9vBO.json",
+});
